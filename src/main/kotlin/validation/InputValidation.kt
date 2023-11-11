e3ed497d08803cfb50addc85cbf314c2120b0e2c
jiwon2724 <stopone3119@naver.com>
2023-11-11 22:18:18 +0900

    [feat] 최대 메뉴, 중복 메뉴 예외 케이스 require 구현

--- src/main/kotlin/validation/InputValidation.kt
+++ src/main/kotlin/validation/InputValidation.kt
@@ -8,6 +8,7 @@ class InputValidation {
         require(day.toInt() in VALID_DAY)
     }
 
+    // 주석 작성한 부분 구현 끝난 후 함수로 분리해야해!
     fun validateMenu(orderMenu: String) {
         val validMenu = orderMenu.split(",")
         val orderResult = arrayListOf<String>()
@@ -18,11 +19,19 @@ class InputValidation {
             require(Menu.values().map { it.productName }.contains(productName))
             require((productOrderCount.toIntOrNull() != null) && (productOrderCount.toInt() > 0))
         }
+        // 음료만 주문시
         val beverage = Menu.values().filter { it.type == "음료" }.map { it.productName }
         require(!orderResult.all { beverage.contains(it) })
+
+        // 메뉴 최대 20개
+        require(orderResult.size <= MAX_ORDER_COUNT)
+
+        // 중복 메뉴
+        require(orderResult.size == orderResult.distinct().size)
     }
 
     companion object {
         private val VALID_DAY = 1..31
+        private const val MAX_ORDER_COUNT = 20
     }
 }
