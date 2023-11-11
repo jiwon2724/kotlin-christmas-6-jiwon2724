0bf2d151fc99d1734c7396f24001cb169e9e8b7b
jiwon2724 <stopone3119@naver.com>
2023-11-11 21:57:11 +0900

    [refactor] toOrderMenu 함수 개선

--- src/main/kotlin/extensions/ConvertExtensions.kt
+++ src/main/kotlin/extensions/ConvertExtensions.kt
@@ -6,10 +6,10 @@ import model.OrderMenu
 fun String.toOrderMenu(): List<Menu> {
     val orderMenu = arrayListOf<Menu>()
     split(",").forEach { menu ->
-        val menuAndCount = menu.split("-")
-        val productName = menuAndCount[0]
-        val productOrderCount = menuAndCount[1].toInt()
-        val product = Menu.valueOf(productName).apply { count = productOrderCount }
+        val (productName, productOrderCount) = menu.split("-")
+        val product = Menu.values()
+            .first { it.productName == productName }
+            .apply { count = productOrderCount.toInt() }
         orderMenu.add(product)
     }
     return orderMenu
