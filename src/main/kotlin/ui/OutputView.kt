a4f078a7a4518eac07ea231e47d9e153586a4b25
jiwon2724 <stopone3119@naver.com>
2023-11-12 23:05:47 +0900

    [chore] 출력 포맷 수정

--- src/main/kotlin/ui/OutputView.kt
+++ src/main/kotlin/ui/OutputView.kt
@@ -1,8 +1,9 @@
 package ui
 
-import domain.BadgeType
+import extensions.toFormattedMoney
+import model.BadgeType
 import model.BenefitType
-import model.menu.Menu
+import model.menu.OrderMenu
 
 class OutputView {
     fun printWelcome() = println(WELCOME)
@@ -10,10 +11,10 @@ class OutputView {
     fun promptMenuAndCount() = println(ORDER_MENU_AND_COUNT)
     fun printEventPreview(day: Int) = println(EVENT_BENEFIT_PREVIEW.format(day))
 
-    fun printOrderMenus(orderMenu: List<Menu>) {
+    fun printOrderMenus(orderMenu: List<OrderMenu>) {
         println(ORDER_MENU_HEADER)
-        orderMenu.forEach { menu ->
-            println(MENU_FORMAT.format(menu.productName, menu.count))
+        orderMenu.forEach { order ->
+            println(MENU_FORMAT.format(order.menu.menuName, order.count))
         }
         println()
     }
@@ -24,32 +25,37 @@ class OutputView {
     }
 
 
-    fun printPresentMenu(presentMenu: Menu) {
+    fun printPresentMenu(presentMenu: OrderMenu) {
         println(PRESENT_MENU_HEADER)
         if (presentMenu.count == 1) {
-            println("${MENU_FORMAT.format(presentMenu.productName, presentMenu.count)}\n")
+            println("${MENU_FORMAT.format(presentMenu.menu.menuName, presentMenu.count)}\n")
             return
         }
         println(NOTHING)
+        println()
     }
 
     fun printBenefitDetails(discount: List<Int>) {
         println(BENEFIT_DETAILS_HEADER)
+        if (discount.all { it == 0 }) {
+            println(NOTHING)
+            println()
+            return
+        }
         BenefitType.values().forEachIndexed { index, benefit ->
-            if (discount[index] == 0) println(BENEFIT_DETAILS_NOTING.format(benefit.type))
-            if (discount[index] != 0) println(BENEFIT_DETAILS.format(benefit.type, discount[index]))
+            println(BENEFIT_DETAILS.format(benefit.type, discount[index].toFormattedMoney()))
         }
         println()
     }
 
     fun printTotalBenefitAmount(amount: Int) {
         println(TOTAL_BENEFIT_AMOUNT)
-        println("${AMOUNT_FORMAT.format(amount)}\n")
+        println("${AMOUNT_FORMAT.format(amount.toFormattedMoney())}\n")
     }
 
     fun printExpectedPaymentAfterDiscount(payment: Int) {
         println(EXPECTED_PAYMENT_AFTER_DISCOUNT_HEADER)
-        println("${AMOUNT_FORMAT.format(payment)}\n")
+        println("${AMOUNT_FORMAT.format(payment.toFormattedMoney())}\n")
     }
 
     fun printEventBadge(eventBadge: BadgeType) {
@@ -66,13 +72,12 @@ class OutputView {
         private const val WELCOME = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
         private const val RESERVATION_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
         private const val ORDER_MENU_AND_COUNT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
-        private const val EVENT_BENEFIT_PREVIEW = "\n12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
-        private const val ORDER_MENU_HEADER = "<주문 메뉴>"
+        private const val EVENT_BENEFIT_PREVIEW = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
+        private const val ORDER_MENU_HEADER = "\n<주문 메뉴>"
         private const val TOTAL_AMOUNT_HEADER = "<할인 전 총주문 금액>"
         private const val PRESENT_MENU_HEADER = "<증정 메뉴>"
         private const val BENEFIT_DETAILS_HEADER = "<혜택 내역>"
-        private const val BENEFIT_DETAILS = "%s: %d원"
-        private const val BENEFIT_DETAILS_NOTING = "%s: 없음"
+        private const val BENEFIT_DETAILS = "%s: %s원"
         private const val TOTAL_BENEFIT_AMOUNT = "<총혜택 금액>"
         private const val EXPECTED_PAYMENT_AFTER_DISCOUNT_HEADER = "<할인 후 예상 결제 금액>"
         private const val EVENT_BADGE_HEADER = "<12월 이벤트 배지>"
