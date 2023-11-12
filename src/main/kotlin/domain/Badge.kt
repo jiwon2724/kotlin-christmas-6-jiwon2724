caac01ec9ec7f25a8738d3a026548c56ffdcbaf4
jiwon2724 <stopone3119@naver.com>
2023-11-12 22:26:34 +0900

    [chore] 패키지 생성 및 클래스 분리

--- src/main/kotlin/domain/Badge.kt
+++ src/main/kotlin/domain/Badge.kt
@@ -1,5 +1,7 @@
 package domain
 
+import model.BadgeType
+
 class Badge(private val benefitAmount: Int) {
     fun eventBadge(): BadgeType {
         return when (benefitAmount) {
@@ -15,11 +17,4 @@ class Badge(private val benefitAmount: Int) {
         private val TREE_RANGE = 10000..19999
         private val SANTA_RANGE = 20000 ..Int.MAX_VALUE
     }
-}
-
-enum class BadgeType(val type: String) {
-    STAR("별"),
-    TREE("트리"),
-    SANTA("산타"),
-    NOTHING("없음");
 }
---
+++ src/main/kotlin/model/BadgeType.kt
@@ -0,0 +1,8 @@
+package model
+
+enum class BadgeType(val type: String) {
+    STAR("별"),
+    TREE("트리"),
+    SANTA("산타"),
+    NOTHING("없음");
+}
