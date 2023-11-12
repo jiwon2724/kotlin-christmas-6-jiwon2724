b0a055ddc083733b8d411c7c3468393e5892c203
jiwon2724 <stopone3119@naver.com>
2023-11-12 11:12:22 +0900

    [chore] 함수 반환 값 수정

--- src/main/kotlin/domain/Benefit.kt
+++ src/main/kotlin/domain/Benefit.kt
@@ -1,8 +1,8 @@
 package domain
 
 class Benefit(private val reserveDay: Int) {
-    fun christmas(): Int = ((reserveDay-1) * 100) + START_BENEFIT_AMOUNT
-    
+    fun christmas(): Int = -((reserveDay-1) * 100) + START_BENEFIT_AMOUNT
+
     companion object {
         private const val START_BENEFIT_AMOUNT = 1000
         private const val DISCOUNT_AMOUNT = 100
