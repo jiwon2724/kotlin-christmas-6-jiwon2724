73a76c62effa4d71b6d8ec39b9a8b825ac9e24bd
jiwon2724 <stopone3119@naver.com>
2023-11-12 15:25:00 +0900

    [feat] 이벤트 뱃지 부여 함수 구현

---
+++ src/main/kotlin/domain/Badge.kt
@@ -0,0 +1,25 @@
+package domain
+
+class Badge(private val benefitAmount: Int) {
+    fun eventBadge(): BadgeType {
+        return when (benefitAmount) {
+            in STAR_RANGE -> BadgeType.STAR
+            in TREE_RANGE -> BadgeType.TREE
+            in SANTA_RANGE -> BadgeType.SANTA
+            else -> BadgeType.NOTHING
+        }
+    }
+
+    companion object {
+        private val STAR_RANGE = 5000..9999
+        private val TREE_RANGE = 10000..19999
+        private val SANTA_RANGE = 20000 ..Int.MAX_VALUE
+    }
+}
+
+enum class BadgeType(private val type: String) {
+    STAR("별"),
+    TREE("트리"),
+    SANTA("산타"),
+    NOTHING("없음");
+}
