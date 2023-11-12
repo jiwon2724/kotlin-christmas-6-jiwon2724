9bac40c538c91b6e46a05b8b49a3ba060d8f7441
jiwon2724 <stopone3119@naver.com>
2023-11-12 14:56:32 +0900

    [chore] 출력 형식 수정

--- src/main/kotlin/ui/OutputView.kt
+++ src/main/kotlin/ui/OutputView.kt
@@ -1,6 +1,7 @@
 package ui
 
-import model.Benefit
+import domain.Discount
+import model.BenefitType
 import model.Menu
 
 class OutputView {
@@ -32,10 +33,11 @@ class OutputView {
         println(NOTHING)
     }
 
-    fun printBenefitDetails(benefitDetails: List<Benefit>) {
+    fun printBenefitDetails(discount: List<Int>) {
         println(BENEFIT_DETAILS_HEADER)
-        benefitDetails.forEach { benefit ->
-            println(BENEFIT_DETAILS.format(benefit.name, benefit.disCount))
+        BenefitType.values().forEachIndexed { index, benefit ->
+            if (discount[index] == 0) println(BENEFIT_DETAILS_NOTING.format(benefit.type))
+            if (discount[index] != 0) println(BENEFIT_DETAILS.format(benefit.type, discount[index]))
         }
         println()
     }
@@ -64,12 +66,13 @@ class OutputView {
         private const val WELCOME = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
         private const val RESERVATION_DATE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
         private const val ORDER_MENU_AND_COUNT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
-        private const val EVENT_BENEFIT_PREVIEW = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
+        private const val EVENT_BENEFIT_PREVIEW = "\n12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
         private const val ORDER_MENU_HEADER = "<주문 메뉴>"
         private const val TOTAL_AMOUNT_HEADER = "<할인 전 총주문 금액>"
         private const val PRESENT_MENU_HEADER = "<증정 메뉴>"
         private const val BENEFIT_DETAILS_HEADER = "<혜택 내역>"
         private const val BENEFIT_DETAILS = "%s: %d원"
+        private const val BENEFIT_DETAILS_NOTING = "%s: 없음"
         private const val TOTAL_BENEFIT_AMOUNT = "<총혜택 금액>"
         private const val EXPECTED_PAYMENT_AFTER_DISCOUNT_HEADER = "<할인 후 예상 결제 금액>"
         private const val EVENT_BADGE_HEADER = "<12월 이벤트 배지>"
