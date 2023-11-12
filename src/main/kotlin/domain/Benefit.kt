1ee35d65d316edca0c6ebfbd1c1746b9f7810b8f
jiwon2724 <stopone3119@naver.com>
2023-11-12 12:13:41 +0900

    [feat] 평일 할인 함수 구현 완료

--- src/main/kotlin/domain/Benefit.kt
+++ src/main/kotlin/domain/Benefit.kt
@@ -9,16 +9,26 @@ class Benefit(
     private val orderMenu: List<Menu>
 ) {
     fun christmas(): Int {
-        if (EventDay.CHRISTMAS.day.contains(reserveDay)) return -((reserveDay-1) * 100) + START_BENEFIT_AMOUNT
+        if (EventDay.CHRISTMAS.day.contains(reserveDay)) return -((reserveDay-1) * 100) + CHRISTMAS_BENEFIT_AMOUNT
         return NOTHING
     }
-    fun weekDay() {
 
+    fun weekDay(): Int {
+        // type도 enum class로 리팩토링 하기
+        if (EventDay.WEEKDAY.day.contains(reserveDay)) {
+            val dessertCount = orderMenu.count { it.type == "디저트" }
+            return dessertCount * DAY_DISCOUNT
+        }
+        return NOTHING
     }
 
     companion object {
-        private const val START_BENEFIT_AMOUNT = 1000
-        private const val DISCOUNT_AMOUNT = 100
+        private const val CHRISTMAS_BENEFIT_AMOUNT = 1000
+        private const val CHRISTMAS_DISCOUNT_AMOUNT = 100
+
+        private const val DAY_DISCOUNT = 2023
+        private const val SPECIAL_DISCOUNT = 1000
+
         private const val NOTHING = 0
     }
 }
