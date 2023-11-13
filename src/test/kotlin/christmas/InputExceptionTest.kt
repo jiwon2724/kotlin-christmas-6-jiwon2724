d0130f4d99f481aec5403d39b0b4cea6f09f8abd
jiwon2724 <stopone3119@naver.com>
2023-11-13 22:16:21 +0900

    [test] 예외 케이스 분리

--- src/test/kotlin/christmas/InputExceptionTest.kt
+++ src/test/kotlin/christmas/InputExceptionTest.kt
@@ -5,18 +5,28 @@ import org.junit.jupiter.api.assertThrows
 import validation.InputValidation
 
 class InputExceptionTest {
-    @Test
-    fun `입력한 날짜가 숫자나 지정된 범위의 날짜가 아니면 예외가 발생한다`() {
-        val exceptionCase = listOf(
-            listOf("1, 2, 3, 1day"), // 문자열
-            listOf("1, 2, 3, -1"), // 음수
-            listOf("1, 2, 3, 32") // 범위에 없는 수
-        )
-        assertThrows<IllegalArgumentException> {
-            exceptionCase.forEach {
-                it.forEach { case ->
-                    InputValidation().validateDay(case)
-                }
+    class InputValidationTest {
+        @Test
+        fun `날짜에 문자열이 입력된 경우 예외가 발생한다`() {
+            val input = "1day"
+            assertThrows<IllegalArgumentException> {
+                InputValidation().validateDay(input)
+            }
+        }
+
+        @Test
+        fun `날짜에 음수가 입력된 경우 예외가 발생한다`() {
+            val input = "-1"
+            assertThrows<IllegalArgumentException> {
+                InputValidation().validateDay(input)
+            }
+        }
+
+        @Test
+        fun `날짜 범위에 없는 수가 입력된 경우 예외가 발생한다`() {
+            val input = "32"
+            assertThrows<IllegalArgumentException> {
+                InputValidation().validateDay(input)
             }
         }
     }
