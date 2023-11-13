c89814b540ea085889ad9662477d64782b8943a3
jiwon2724 <stopone3119@naver.com>
2023-11-13 15:45:32 +0900

    [feat] 총혜택 금액, 할인 후 예쌍 결제 금액 구현

--- src/main/kotlin/domain/Amount.kt
+++ src/main/kotlin/domain/Amount.kt
@@ -1,9 +1,21 @@
 package domain
 
 import extensions.payment
-import model.menu.Menu
+import model.benefit.BenefitType
 import model.menu.OrderMenu
 
-class Amount(private val orderMenu: List<OrderMenu>) {
+class Amount(
+    private val orderMenu: List<OrderMenu>,
+    private val benefit: Benefit
+) {
     fun beforeDiscountAmount(): Int = orderMenu.payment()
+
+    fun totalBenefitAmount(): Int = benefit
+        .allBenefit()
+        .sumOf { it.disCount }
+
+    fun afterDiscountAmount(): Int = benefit
+        .allBenefit()
+        .filter { it.type != BenefitType.PRESENT_EVENT }
+        .sumOf { it.disCount }
 }
