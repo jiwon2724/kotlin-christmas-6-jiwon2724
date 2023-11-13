8d08522d8885e0656d3eddea65d771aabb9f6da0
jiwon2724 <stopone3119@naver.com>
2023-11-13 21:41:04 +0900

    [chore] 함수 로직 수정

--- src/main/kotlin/validation/InputValidation.kt
+++ src/main/kotlin/validation/InputValidation.kt
@@ -5,22 +5,22 @@ import model.menu.MenuType
 
 class InputValidation {
     fun validateDay(day: String) {
-        require((day.toIntOrNull() != null) && (day.toInt() > 0))
+        require(day.toIntOrNull() != null)
         require(day.toInt() in VALID_DAY)
     }
 
     fun validateMenu(orderMenu: String) {
-        val orderResult = arrayListOf<String>()
+        val orderResult = arrayListOf<Pair<String, Int>>()
         orderMenu.split(",").forEach { menu ->
             require(menu.contains("-"))
             val (menuName, productOrderCount) = menu.split("-")
-            orderResult.add(menuName)
+            orderResult.add(Pair(menuName, productOrderCount.toInt()))
             require(Menu.values().map { it.menuName }.contains(menuName))
-            require((productOrderCount.toIntOrNull() != null) && (productOrderCount.toInt() < 0))
+            require((productOrderCount.toIntOrNull() != null) && (productOrderCount.toInt() > 0))
         }
         val beverage = Menu.values().filter { it.type == MenuType.BEVERAGE }.map { it.menuName }
-        require(!orderResult.all { beverage.contains(it) })
-        require(orderResult.size <= MAX_ORDER_COUNT)
+        require(!orderResult.all { beverage.contains(it.first) })
+        require(orderResult.sumOf { it.second } <= MAX_ORDER_COUNT)
         require(orderResult.size == orderResult.distinct().size)
     }
 
