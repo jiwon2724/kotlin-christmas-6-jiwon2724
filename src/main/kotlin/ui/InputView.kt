0b7fc23ddf07680a1ef5d778de9d713b2ae2db4d
jiwon2724 <stopone3119@naver.com>
2023-11-13 21:40:28 +0900

    [chore] 람다 적용 롤백

--- src/main/kotlin/ui/InputView.kt
+++ src/main/kotlin/ui/InputView.kt
@@ -4,29 +4,27 @@ import camp.nextstep.edu.missionutils.Console
 import validation.InputValidation
 
 class InputView(private val inputValidation: InputValidation = InputValidation()) {
-    fun readDay(errorMessage: () -> Unit): String = handleException(errorMessage) {
-        Console.readLine().let { day ->
-            inputValidation.validateDay(day)
-            day
-        }
-    }
-
-
-    fun readMenu(errorMessage: () -> Unit): String = handleException(errorMessage) {
-        Console.readLine().let { menu ->
-            inputValidation.validateMenu(menu)
-            menu
-        }
-    }
-
-    companion object {
-        private fun handleException(errorMessage: () -> Unit, validate: () -> String): String {
-            return try {
-                validate()
-            } catch (e: IllegalArgumentException) {
-                errorMessage()
-                validate()
+    fun readDay(errorMessage: () -> Unit): String {
+        return try {
+            Console.readLine().let { inputDay ->
+                inputValidation.validateDay(inputDay)
+                inputDay
             }
+        } catch (e: IllegalArgumentException) {
+            errorMessage()
+            readDay(errorMessage)
+        }
+    }
+
+    fun readMenu(errorMessage: () -> Unit): String {
+        return try {
+            Console.readLine().let { inputMenu ->
+                inputValidation.validateMenu(inputMenu)
+                inputMenu
+            }
+        } catch (e: IllegalArgumentException) {
+            errorMessage()
+            readMenu(errorMessage)
         }
     }
 }
