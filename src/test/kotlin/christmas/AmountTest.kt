3a9e4e072ac4a4819f5f3cf5db1e688b32e21118
jiwon2724 <stopone3119@naver.com>
2023-11-14 13:09:54 +0900

    [test] 금액에 대한 테스트 코드 작성

--- src/test/kotlin/christmas/AmountTest.kt
+++ src/test/kotlin/christmas/AmountTest.kt
@@ -12,10 +12,25 @@ import org.junit.jupiter.api.Test
 class AmountTest {
     @Test
     fun `할인 전 총 주문 금액이 맞는지 확인`() {
-        assertEquals(200000, Amount(orderMenu = ORDER_MENU, benefit = BENEFIT).beforeDiscountAmount())
+        val amount = Amount(orderMenu = ORDER_MENU, benefit = BENEFIT)
+        assertEquals(200000, amount.beforeDiscountAmount())
     }
 
-    
+    @Test
+    fun `총 혜택 금액이 맞는지 확인`() {
+        // 크리스마스 디데이 할인 : -1200
+        // 특별 할인 : -1000
+        // 증정 이벤트 -25000
+        // 평일 할인 : -6069
+        val amount = Amount(orderMenu = ORDER_MENU, benefit = BENEFIT)
+        assertEquals(-33269, amount.totalBenefitAmount())
+    }
+
+    @Test
+    fun `할인 후 금액이 맞는지 확인`() {
+        val amount = Amount(orderMenu = ORDER_MENU, benefit = BENEFIT)
+        assertEquals(191731, amount.beforeDiscountAmount() + amount.afterDiscountAmount())
+    }
 
     companion object {
         private val ORDER_MENU = listOf(
@@ -30,6 +45,6 @@ class AmountTest {
             orderMenu = ORDER_MENU,
             expectDay = 3,
             present = PRESENT
-        )
+        ).apply { setBeforeDiscountAmount(200000) }
     }
 }
